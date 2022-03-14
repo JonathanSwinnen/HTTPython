a6f4402ce19a8b32954744d11ecad0d68524ccae
Andres Van Schel <r0806556@esat.kuleuven.be>
2022-03-14 17:53:43 +0100

    stripping of user input and function for sending request to server

---
+++ HTTP_CLIENT.py
@@ -0,0 +1,22 @@
+import socket
+PORT = 80
+
+input = input("HTTP request: ")
+http_command, URI = input.split(" ")[:2]
+stripped_URI = URI.split("/", 3)
+HOST = stripped_URI[2]
+PATH = "/" + stripped_URI[3]
+print(http_command, URI)
+print(HOST, PATH)
+
+
+def send_request(http_command, HOST, PATH):
+    request = http_command + PATH + "HTTP/1.1"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(("127.0.0.1", PORT))
+        s.send(request)
+        data = s.recv(1024)
+        all_data = data
+        while len(data) == 1024:
+            data = s.recv(1024)
+            all_data += data
