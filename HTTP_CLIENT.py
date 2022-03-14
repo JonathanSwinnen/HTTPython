4bed4f517de18e5dbecd570e65f96f0df6b15ef6
Jonathan Swinnen <46630860+JonathanSwinnen@users.noreply.github.com>
2022-03-14 17:59:31 +0100

    bugfix

--- HTTP_CLIENT.py
+++ HTTP_CLIENT.py
@@ -14,9 +14,11 @@ def send_request(http_command, HOST, PATH):
     request = http_command + PATH + "HTTP/1.1"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("127.0.0.1", PORT))
-        s.send(request)
+        s.send(bytes(request, 'UTF-8'))
         data = s.recv(1024)
         all_data = data
         while len(data) == 1024:
             data = s.recv(1024)
             all_data += data
+
+send_request(http_command, 0, "/")
