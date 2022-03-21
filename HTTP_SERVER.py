755dc9693792c4236d2094deff71007bd9709ef5
Jonathan Swinnen <46630860+JonathanSwinnen@users.noreply.github.com>
2022-03-21 20:24:47 +0100

    cleaned up a (very little) bit implemented basic PUT, POST and HEAD functionality and 404 errors

--- HTTP_SERVER.py
+++ HTTP_SERVER.py
@@ -1,35 +1,97 @@
 # import socket programming library
 import socket
 import mimetypes
+from urllib.parse import urlparse
+import os
+from email.utils import formatdate
+
 
 # import thread module
 from _thread import *
 import threading
 
 # thread function
-def threaded(c):
+def handle_connection(c):
+    print("\n--- CONNECTION: STARTED THREAD ---")
+    method, uri, httpv, headers, total = read_head(c)
+    print("got request: \n" + total)
+    # if not full uri (form: http://server/path) --> convert so we can use urlparse
 
-    # loop & receive data
-    print("started thread")
+    if uri.startswith("/"):
+        if uri == "/":
+            uri = "/index.html"
+        uri = "http://" + c.getsockname()[0] + uri
+    parsed_uri = urlparse(uri)
+    path = os.getcwd() + "/web" + parsed_uri.path
+
+    if method == "GET" or method == "HEAD":
+        response = "HTTP/1.1 "
+        content = ""
+        content_type = 'text/html'
+        print("get path: " + path)
+        if not os.path.isfile(path):
+            print("not found!!")
+            response += "404 Not Found"
+            content = "Sorry! The requested file was not found on our server :(".encode()
+        else:
+            response += '200 OK'
+            content_type = mimetypes.guess_type(uri)[0] or 'text/html'
+            fin = open(path, "rb")
+            content = fin.read()
+            fin.close()
+
+        response += "\r\nContent-Type: " + content_type
+        response += "\r\nDate: " + formatdate(timeval=None, localtime=False, usegmt=True)
+        response += "\r\n\r\n"
+
+        c.send(response.encode())
+        if method == "GET":
+            c.sendall(content)
+
+    elif method == "POST" or method == "PUT":
+        cl = int(headers.get("content-length"))
+        body = ""
+        if cl is None:
+            print("no content-length given!")
+            pass  # bad request
+        else:
+            while len(body) < cl:
+                body += c.recv(cl).decode()
+                print("reading body: " + body)
+            if not len(body) == cl:
+                print("body size does not equal content-length!")
+                pass  # bad request
+
+        print("writing to: " + parsed_uri.path)
+        if method == "POST":
+            with open(path, "a") as f:
+                f.write(body)
+        else:
+            with open(path, "w+") as f:
+                f.write(body)
+        print("done writing")
+    c.close()
+    print("--- CONNECTION CLOSED ---")
+
+def read_head(c):
     req = ""
     method = ""
     uri = ""
     httpv = ""
     total = ""
     headers = dict()
-    while True:
-
-        print("-- READ LOOP: NEW CHUNK")
+    reading_head = True
+    while reading_head:
         # receive
         try:
-            data_chunk = c.recv(1024)
+            data_chunk = c.recv(1)
         except c.timeout as e:
             err = e.args[0]
             # this next if/else is a bit redundant, but illustrates how the
             # timeout exception is setup
             if err == 'timed out':
-                print('recv timed out, retry later')
-                continue
+                print('TIMEOUT')
+                break
             else:
                 print(e)
                 break
@@ -37,78 +99,59 @@ def threaded(c):
             # Something else happened, handle error, exit, etc.
             print(e)
             break
-
         req += data_chunk.decode()
         total += data_chunk.decode()
-        print("carry + chunk received ::\n\n" + req)
-        print("\n::end received")
         recv_lines = req.split("\r\n")
-
         # parse received lines
         i = 0
         for line in recv_lines:
             i += 1
-            if i == len(recv_lines):
+            if i == len(recv_lines):  # last line
                 if line == "":  # completed line at the end
-                    print("finished with complete line, ... clearing")
                     req = ""
-                else:           # incomplete line at the end
-                    print("incomplete line ... carrying over")
-                    req = line # continue receiving last request line
+                else:  # incomplete line at the end
+                    req = line  # continue receiving last request line
             elif method == "":  # expecting Request-Line (method, uri, http version)
                 req_line = line.split(" ")
-                if req_line[0] == "GET":
-                    method = "GET"
-                    uri = req_line[1]
-                    httpv = req_line[2]
-                    print("read GET request: " + line + "   , expecting headers ...")
-                else:
-                    pass  # !!! only GET is implemented
+                method = req_line[0]
+                uri = req_line[1]
+                httpv = req_line[2]
+                print("\nread request-line: " + line + "   , expecting headers ...")
             else:  # expecting headers or newline to end
                 if line != "":
-                    print("reading header: " + line)
+                    print("\nreading header: " + line)
                     header_line = line.split(":", 1)
                     print(header_line)
-                    headers[header_line[0]] = header_line[1].strip()
-                else:
-                    print("END parsing")
-                    print(total)
-                    respond(c, method, uri, httpv, headers)
-                    method = ""
-                    uri = ""
-                    headers = dict()
-                    httpv = ""
-                    req = ""
-                    c.close()
-                    return
-        print(method)
-        print(uri)
-        print(httpv)
-        print(headers)
-    print("ended connection")
+                    headers[header_line[0].lower()] = header_line[1].strip()
+                else:  # CRLF after headers --> end request or body
+                    print("END HEADERS\n")
+                    reading_head = False
+    return method, uri, httpv, headers, total
 
 
-def respond(c, method, uri, httpv, headers):
-    if method == "GET":
-        print("responding to GET request")
-        if uri == '/':
-            uri = '/index.html'
-        content_type = mimetypes.guess_type(uri)[0] or 'text/html'
 
-        fin = open('webpage' + uri, "rb")
-        content = fin.read()
-        fin.close()
-        response = ('HTTP/1.0 200 OK\nContent-Type: '+content_type+'\n\n').encode() + content
-        c.sendall(response)
+def respond(c, method, uri, httpv, headers, body):
+
+    print("responding with header")
+
+
+
+def getmyip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
 
 def Main():
-    host = "127.0.0.1"
+    host = getmyip()
     port = 8000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.settimeout(2)
-    print("socket binded to port", port)
+    print("socket binded @ " + host + ":"+str(port))
     # put the socket into listening mode
     s.listen()
     print("socket is listening")
@@ -123,7 +166,8 @@ def Main():
                 continue
             print('Connected to :', a[0], ':', a[1])
             # Start a new thread and return its identifier
-            start_new_thread(threaded, (c,))
+            #start_new_thread(handle_connection, (c,))
+            handle_connection(c)
         s.close()
     except KeyboardInterrupt:
         pass
@@ -132,3 +176,5 @@ def Main():
 
 if __name__ == '__main__':
     Main()
+
+
